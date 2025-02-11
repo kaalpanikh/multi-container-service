e39ead64fb8d4f6e9555fb20a0320f8d02250b07
kaalpanikh <nm30472@gmail.com>
2025-02-11 15:46:00 +0530

    Add debug output and simplify Jest config

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  verbose: true,
-  setupFilesAfterEnv: ['./__tests__/setup.js']
+  testMatch: ['**/test/**/*.test.js'],
+  verbose: true
 };
